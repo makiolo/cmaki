26c0c21d1563abc3235bf4a9d3c3350ff9387383
Ricardo Marmolejo Garcia <makiolo@gmail.com>
2015-11-18 23:43:59 +0100

    remove files

--- Findverypoco.cmake
+++
@@ -1,8 +0,0 @@
-# find_package
-include_directories(SYSTEM ../verypoco/include)
-if(WIN32)
-	add_definitions(-DPOCO_OS_FAMILY_WINDOWS)
-endif()
-add_definitions(-DUNICODE -D_UNICODE)
-add_definitions(-DPOCO_NO_AUTOMATIC_LIBS -DPOCO_DLL)
-
