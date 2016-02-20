c6a8c04a6aaba89ebe66625f0b5e447acc3bbb49
Ricardo Marmolejo Garcia <makiolo@gmail.com>
2016-02-20 11:48:25 +0100

    add scripts for search and replace

--- facts/facts.cmake
+++ facts/facts.cmake
@@ -122,7 +122,7 @@ function(cmaki_find_package PACKAGE)
 			# generar artefactos de una version determinada
 
 			execute_process(
-				COMMAND python ${ARTIFACTS_PATH}/build.py ${PACKAGE} --depends=${CMAKI_PATH}/../depends.json --cmakefiles=${CMAKI_PATH} --prefix=${CMAKE_PREFIX_PATH} --third-party-dir=${CMAKE_PREFIX_PATH} -o -d
+				COMMAND python ${ARTIFACTS_PATH}/build.py ${PACKAGE} --depends=${CMAKI_PATH}/../depends.json --cmakefiles=${CMAKI_PATH} --prefix=${CMAKE_PREFIX_PATH} --third-party-dir=${CMAKE_PREFIX_PATH} -o
 				WORKING_DIRECTORY "${ARTIFACTS_PATH}"
 				RESULT_VARIABLE artifacts_result
 				)
