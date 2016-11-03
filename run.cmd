367c8cdbcf6fca6df6d6d64217006248ab1ae60a
Ricardo Marmolejo García <makiolo@gmail.com>
2016-11-03 22:22:08 +0100

    add run.cmd

---
+++ run.cmd
@@ -0,0 +1,11 @@
+@echo off
+set GENERATOR=Visual Studio 14 2015
+set MODE=Debug
+echo "running in mode %MODE% ..."
+md %MODE%
+cd %MODE%
+cmake .. -DCMAKE_BUILD_TYPE=%MODE% -DFIRST_ERROR=1 -G"%GENERATOR%"
+cmake --build . --config %MODE% --target install
+ctest . --no-compress-output --output-on-failure -T Test -C %MODE% -V
+cd ..
+
