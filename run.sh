b75090accf46dacb8d4af6e7a3a76e3717d80073
Ricardo <makiolo@gmail.com>
2016-11-15 17:19:14 +0100

    add NOCACHE env var

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 set -e
 MODE=${1:-Debug}
 COMPILER=${2:-clang}
+"${NOCACHE:?FALSE}"
 if [[ $COMPILER == "clang" ]];
 then
 	export CC=clang-3.6
@@ -10,14 +11,6 @@ else
 	export CC=gcc-4.9
 	export CXX=g++-4.9
 fi
-# if [ -f "conanfile.txt" ];
-# then
-# 	if [ ! -f "conanbuildinfo.cmake" ];
-# 	then
-# 		# fetch depends and generate conan cmake file
-# 		conan install
-# 	fi
-# fi
 
 if [[ "$OSTYPE" == "cygwin" ]]; then
 	PATH=$(cygpath "E:\dev\gtc1\bin\cmake-3.4.3-win32-x86\bin"):$PATH
@@ -31,8 +24,7 @@ fi
 echo "running in mode $MODE ... ($CC / $CXX)"
 mkdir -p $CC/$MODE
 cd $CC/$MODE
-cmake ../.. -DCMAKE_BUILD_TYPE=$MODE -DCMAKE_MODULE_PATH=$(pwd)/../../cmaki -DFIRST_ERROR=1 -G"$GENERATOR" -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX"
-# cmake ../.. -DCMAKE_BUILD_TYPE=$MODE -DCMAKE_MODULE_PATH=$(pwd)/../../cmaki -DFIRST_ERROR=1 -G"$GENERATOR" -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -DNOCACHE=TRUE
+cmake ../.. -DCMAKE_BUILD_TYPE=$MODE -DCMAKE_MODULE_PATH=$(pwd)/../../cmaki -DFIRST_ERROR=1 -G"$GENERATOR" -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -DNOCACHE=$NOCACHE
 cmake --build . --config $MODE --target install -- -j8 -k || cmake --build . --config $MODE --target install -- -j1
 ctest . --no-compress-output --output-on-failure -T Test -C $MODE -V
 
