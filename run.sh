85ba68d3df946b830d45c2affa3fd476abf59534
Ricardo <makiolo@gmail.com>
2016-11-16 10:20:02 +0100

    add flag no cache remote

--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@ set -e
 MODE=${1:-Debug}
 COMPILER=${2:-clang}
 "${NOCACHE:?FALSE}"
+"${NOCACHE_LOCAL:?FALSE}"
 if [[ $COMPILER == "clang" ]];
 then
 	export CC=clang-3.6
@@ -24,7 +25,7 @@ fi
 echo "running in mode $MODE ... ($CC / $CXX)"
 mkdir -p $CC/$MODE
 cd $CC/$MODE
-cmake ../.. -DCMAKE_BUILD_TYPE=$MODE -DCMAKE_MODULE_PATH=$(pwd)/../../cmaki -DFIRST_ERROR=1 -G"$GENERATOR" -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -DNOCACHE=$NOCACHE
+cmake ../.. -DCMAKE_BUILD_TYPE=$MODE -DCMAKE_MODULE_PATH=$(pwd)/../../cmaki -DFIRST_ERROR=1 -G"$GENERATOR" -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -DNOCACHE=$NOCACHE -DNOCACHE_LOCAL=$NOCACHE_LOCAL
 cmake --build . --config $MODE --target install -- -j8 -k || cmake --build . --config $MODE --target install -- -j1
 ctest . --no-compress-output --output-on-failure -T Test -C $MODE -V
 
