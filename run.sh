9a29ffea1f87e15da5955fe84c68b745c02ed9fa
Ricardo Marmolejo Garcia <makiolo@gmail.com>
2016-12-06 19:30:25 +0100

    minor fix

--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ set -e
 MODE=${1:-Debug}
 NOCACHE_REMOTE="${NOCACHE_REMOTE:-FALSE}"
 NOCACHE_LOCAL="${NOCACHE_LOCAL:-FALSE}"
+CC="${CC:-gcc}"
+CXX="${CXX:-g++}"
 
 if [[ "$OSTYPE" == "cygwin" ]]; then
 	PATH=$(cygpath "E:\dev\gtc1\bin\cmake-3.4.3-win32-x86\bin"):$PATH
@@ -16,7 +18,7 @@ fi
 echo "running in mode $MODE ... ($CC / $CXX)"
 mkdir -p $CC/$MODE
 cd $CC/$MODE
-cmake ../.. -DCMAKE_BUILD_TYPE=$MODE -DCMAKE_MODULE_PATH=$(pwd)/../../cmaki -DFIRST_ERROR=1 -G"$GENERATOR" -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -DNOCACHE_REMOTE=$NOCACHE_REMOTE -DNOCACHE_LOCAL=$NOCACHE_LOCAL
+cmake ../.. -DCMAKE_BUILD_TYPE=$MODE -DFIRST_ERROR=1 -G"$GENERATOR" -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -DNOCACHE_REMOTE=$NOCACHE_REMOTE -DNOCACHE_LOCAL=$NOCACHE_LOCAL
 cmake --build . --config $MODE --target install -- -j8 -k || cmake --build . --config $MODE --target install -- -j1
 ctest . --no-compress-output --output-on-failure -T Test -C $MODE -V
 
