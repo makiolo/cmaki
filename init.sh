9ba81d75df939363eed360abc9d7bd4b5b1c25b8
Ricardo Marmolejo Garcia <makiolo@gmail.com>
2016-12-07 21:48:15 +0100

    improve init script

--- init.sh
+++ init.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-MY_PATH="`dirname \"$0\"`"
+PWD="`dirname \"$0\"`"
 
-cp -v $MY_PATH/init/.travis.yml .
+cp -v $PWD/init/.travis.yml .
+git add .travis.yml
+
+cp -v $PWD/init/appveyor.yml .
+git add appveyor.yml
+
+cp -v $PWD/init/.clang-format .
+git add .clang-format
+
+cp -v $PWD/init/.gitignore .
+git add .gitignore
+
+cp -v $PWD/init/api.h .
+git add api.h
 
