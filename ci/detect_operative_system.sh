6f21adb0bdc0baa13cba57dd148cc5453dc59b65
Ricardo Marmolejo García <makiolo@gmail.com>
2017-01-29 14:03:15 +0100

    add debug mode in platform variable

--- ci/detect_operative_system.sh
+++ ci/detect_operative_system.sh
@@ -5,7 +5,8 @@
 OS=`uname -s`
 REV=`uname -r`
 MACH=`uname -m`
-CC="${CC:-default}"
+MODE="${MODE:-UNDEFINED}"
+CC="${CC:-UNDEFINED}"
 CC=$(basename $CC)
 OSSTR=$(uname | tr "[:upper:]" "[:lower:]")
 if [ "${OS}" = "SunOS" ] ; then
@@ -17,32 +18,40 @@ elif [ "${OS}" = "AIX" ] ; then
 elif [ "${OS}" = "Linux" ] ; then
 	KERNEL=`uname -r`
 	if [ -f /etc/redhat-release ] ; then
-	DIST='Redhat'
-	PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
-	REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
+		DIST='Redhat'
+		PSUEDONAME=`cat /etc/redhat-release | sed s/.*\(// | sed s/\)//`
+		REV=`cat /etc/redhat-release | sed s/.*release\ // | sed s/\ .*//`
 	elif [ -f /etc/SuSE-release ] ; then
-	DIST=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
-	REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //`
+		DIST=`cat /etc/SuSE-release | tr "\n" ' '| sed s/VERSION.*//`
+		REV=`cat /etc/SuSE-release | tr "\n" ' ' | sed s/.*=\ //`
 	elif [ -f /etc/mandrake-release ] ; then
-	DIST='Mandrake'
-	PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
-	REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
+		DIST='Mandrake'
+		PSUEDONAME=`cat /etc/mandrake-release | sed s/.*\(// | sed s/\)//`
+		REV=`cat /etc/mandrake-release | sed s/.*release\ // | sed s/\ .*//`
 	elif [ -f /etc/debian_version ] ; then
-	#DIST="Debian_`cat /etc/debian_version | tr "/" '_'`"
-	if [ -f "/etc/lsb-release" -o -d "/etc/lsb-release.d" ] ; then
-		DIST=$(lsb_release -ic | cut -d: -f2 | sed s/'^\t'// | tr '\n' '_' | sed s/'_$'//)
-	else
-		DIST=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1 | tr '\n' '_' | sed s/'_$'//)
+		#DIST="Debian_`cat /etc/debian_version | tr "/" '_'`"
+		if [ -f "/etc/lsb-release" -o -d "/etc/lsb-release.d" ] ; then
+			DIST=$(lsb_release -ic | cut -d: -f2 | sed s/'^\t'// | tr '\n' '_' | sed s/'_$'//)
+		else
+			DIST=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1 | tr '\n' '_' | sed s/'_$'//)
+		fi
+		REV=""
 	fi
-	REV=""
 
-	fi
 	if [ -f /etc/UnitedLinux-release ] ; then
-	DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
+		DIST="${DIST}[`cat /etc/UnitedLinux-release | tr "\n" ' ' | sed s/VERSION.*//`]"
 	fi
 
-	OSSTR="${OS}_${DIST}_${REV}${PSUEDONAME}${MACH}_${CC}"
+	OSSTR="${OS}_${DIST}_${REV}${PSUEDONAME}${MACH}"
 fi
 
-echo $OSSTR
+if [ "$CC" == "UNDEFINED" ]; then
+	echo "${OSSTR}"
+else
+	if [ "$MODE" == "UNDEFINED" ]; then
+		echo "${OSSTR}_${CC}"
+	else
+		echo "${OSSTR}_${CC}_${MODE}"
+	fi
+fi
 
