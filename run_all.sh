18365cf9920dc9ad1f1b8898197a6dd40ee0751d
Ricardo Marmolejo García <makiolo@gmail.com>
2017-02-17 20:43:52 +0100

    .

--- run_all.sh
+++ run_all.sh
@@ -4,7 +4,7 @@ CC=gcc CXX=g++ MODE=Release ./cmaki/run.sh
 CC=clang CXX=clang++ MODE=Debug ./cmaki/run.sh
 CC=clang CXX=clang++ MODE=Release ./cmaki/run.sh
 
-TRAVIS_REPO_SLUG=makiolo/design-patterns-cpp14
+TRAVIS_REPO_SLUG=makiolo/$(basename $(pwd))
 (cd cmaki_generator && CC=gcc CXX=g++ MODE=Debug ./build github://$TRAVIS_REPO_SLUG -o --server=http://artifacts.myftp.biz:8080)
 (cd cmaki_generator && CC=gcc CXX=g++ MODE=Release ./build github://$TRAVIS_REPO_SLUG -o --server=http://artifacts.myftp.biz:8080)
 (cd cmaki_generator && CC=clang CXX=clang++ MODE=Debug ./build github://$TRAVIS_REPO_SLUG -o --server=http://artifacts.myftp.biz:8080)
