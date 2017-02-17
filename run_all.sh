a599a28abb2dbc139a452cd9ce56ad8da7ef71f0
Ricardo Marmolejo García <makiolo@gmail.com>
2017-02-17 20:35:49 +0100

    improve run_all

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/bin/bash -e
 CC=gcc CXX=g++ MODE=Debug ./cmaki/run.sh
 CC=gcc CXX=g++ MODE=Release ./cmaki/run.sh
 CC=clang CXX=clang++ MODE=Debug ./cmaki/run.sh
 CC=clang CXX=clang++ MODE=Release ./cmaki/run.sh
+
+TRAVIS_REPO_SLUG=makiolo/design-patterns-cpp14
+(cd cmaki_generator && CC=gcc CXX=g++ MODE=Debug ./build github://$TRAVIS_REPO_SLUG -o --server=http://artifacts.myftp.biz:8080)
+(cd cmaki_generator && CC=gcc CXX=g++ MODE=Release ./build github://$TRAVIS_REPO_SLUG -o --server=http://artifacts.myftp.biz:8080)
+(cd cmaki_generator && CC=clang CXX=clang++ MODE=Debug ./build github://$TRAVIS_REPO_SLUG -o --server=http://artifacts.myftp.biz:8080)
+(cd cmaki_generator && CC=clang CXX=clang++ MODE=Release ./build github://$TRAVIS_REPO_SLUG -o --server=http://artifacts.myftp.biz:8080)
 
