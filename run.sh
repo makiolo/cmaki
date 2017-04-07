a0135169a3728ca68d995fd47b7821a962c3765f
Ricardo <makiolo@gmail.com>
2017-04-07 15:52:40 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -42,4 +42,12 @@ cmake ../.. -DCMAKE_BUILD_TYPE=$MODE -DFIRST_ERROR=1 -G"$GENERATOR" -DCMAKE_C_CO
 # -DCOMPILER_RT_BUILD_SHARED_ASAN=ON
 cmake --build . --config $MODE --target install -- -j8 -k || cmake --build . --config $MODE --target install -- -j1
 ctest . --no-compress-output --output-on-failure -T Test -C $MODE -V
-
+if [[ "$CC" == "gcc" ]]; then
+	if [[ "$MODE" == "Debug" ]]; then
+		# generate coverage reports in gcc Debug
+		lcov --directory . --capture --output-file coverage.info  # capture coverage info
+		lcov --remove coverage.info '/usr/*' --output-file coverage.info  # filter out system
+		lcov --list coverage.info  # debug info
+		bash <(curl -s https://codecov.io/bash) || echo "Codecov did not collect coverage reports"
+	fi
+fi
