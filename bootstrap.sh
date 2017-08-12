0aa30f6d1968ddadb12ee8820d67f3c538e5d35a
Ricardo Marmolejo García <makiolo@gmail.com>
2017-08-12 23:52:05 +0200

    no bootstrap generator

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,8 @@ if [[ -d cmaki ]]; then
 fi
 git clone https://github.com/makiolo/cmaki.git
 
-if [[ -d cmaki_generator ]]; then
-	rm -Rf cmaki_generator
-fi
-git clone https://github.com/makiolo/cmaki_generator.git
+# if [[ -d cmaki_generator ]]; then
+# 	rm -Rf cmaki_generator
+# fi
+# git clone https://github.com/makiolo/cmaki_generator.git
 
