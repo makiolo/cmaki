806b684f2fc822cc82ee8036b7a207c808c8c525
Ricardo Marmolejo García <makiolo@gmail.com>
2017-08-20 13:30:52 +0200

    .

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -e
 
 env | sort
 
-NODE_PATH="${NODE_PATH:-.}"
+NODE_PATH="${NODE_PATH:-$PWD}"
 $NODE_PATH/node_modules/cmaki/clean.sh
 $NODE_PATH/node_modules/cmaki/setup.sh
 $NODE_PATH/node_modules/cmaki/compile.sh
