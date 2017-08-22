25b2ae8560e4081f53df2dae809355ada04047cc
Ricardo <makiolo@gmail.com>
2017-08-22 17:21:50 +0200

    Update compile.cmd

--- compile.cmd
+++ compile.cmd
@@ -7,9 +7,11 @@ if "%Configuration%" == "Release" (
 )
 
 if "%Platform%" == "x64" (
-    set GENERATOR=Visual Studio 14 2015 Win64
+    :: set GENERATOR=Visual Studio 14 2015 Win64
+    set GENERATOR=Visual Studio 15 2017 Win64
 ) else (
-    set GENERATOR=Visual Studio 14 2015
+    :: set GENERATOR=Visual Studio 14 2015
+    set GENERATOR=Visual Studio 15 2017
 )
 
 echo running in mode %MODE% ...
