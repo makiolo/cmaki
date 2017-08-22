6cca98651a94cbe5ecc3498e9041efc780f8f7c8
Ricardo <makiolo@gmail.com>
2017-08-22 16:09:56 +0200

    Update bootstrap.cmd

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -1,1 +1,8 @@
-echo Hola mundo since bootstrap.cmd
+@echo off
+IF EXIST node_modules\cmaki (
+  echo .
+) else (
+  md node_modules\cmaki
+  cd node_modules && git clone https://github.com/makiolo/cmaki.git && cd ..
+  cd node_modules/cmaki && rm -Rf .git && cd ..\..
+)
