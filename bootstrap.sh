213f9f3fb0f071fa1ed43770ca0b2abaab794bf3
Ricardo <makiolo@gmail.com>
2017-08-22 16:07:11 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
 if [[ ! -d node_modules/cmaki ]]; then
 	mkdir -p node_modules/cmaki
 	(cd node_modules && git clone https://github.com/makiolo/cmaki.git)
 	(cd node_modules/cmaki && rm -Rf .git)
 fi
-
