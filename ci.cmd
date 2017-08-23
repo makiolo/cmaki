5d17ee1ba754c7c9293eaec06a0411eed7d300ca
Ricardo <makiolo@gmail.com>
2017-08-23 09:54:42 +0200

    Create ci.cmd

---
+++ ci.cmd
@@ -0,0 +1,9 @@
+@echo off
+if exist node_modules (rmdir /s /q node_modules)
+if %errorlevel% neq 0 exit /b %errorlevel%
+powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki/master/bootstrap.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
+if %errorlevel% neq 0 exit /b %errorlevel%
+call node_modules\cmaki\install.cmd
+if %errorlevel% neq 0 exit /b %errorlevel%
+call node_modules\cmaki\tests.cmd
+if %errorlevel% neq 0 exit /b %errorlevel%
