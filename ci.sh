1b14f476ad760c6f923dc1db9797df3aa5e7dadb
Ricardo <makiolo@gmail.com>
2017-08-23 15:58:18 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -8,13 +8,24 @@ pip install --user poster
 pip install --user codecov
 
 if [ -f "package.json" ]; then
+
+  echo [1/3] prepare
   npm install npm-check-updates
   ./node_modules/.bin/ncu -u
+
+  echo [2/3] compile
   npm install
+
+  echo [3/3] run tests
   npm test
 else
+  echo [1/3] prepare
   curl -s https://raw.githubusercontent.com/makiolo/cmaki/master/bootstrap.sh | bash
+
+  echo [2/3] compile
   ./node_modules/cmaki/install.sh
+
+  echo [3/3] run tests
   ./node_modules/cmaki/tests.sh
 fi
 
