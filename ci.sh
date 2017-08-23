ee81fc00fa92e5a001427ed60853d0c364511720
Ricardo <makiolo@gmail.com>
2017-08-23 10:46:33 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -3,6 +3,13 @@ set -e
 pip install --user pyyaml
 pip install --user poster
 pip install --user codecov
+if [ -f "package.json" ]; then
+  npm install -g npm-check-updates
+  ncu -u
+fi
 bash <(curl -s https://raw.githubusercontent.com/makiolo/cmaki/master/bootstrap.sh)
 CMAKI_PWD=$(pwd) ./node_modules/cmaki/install.sh
 ./node_modules/cmaki/tests.sh
+if [ -f "cmaki.yml" ]; then
+  echo TODO: upload with cmaki_generator
+fi
