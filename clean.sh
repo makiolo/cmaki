68a99dabf33f07b9cfca01418f12cbae7f2b32d8
Ricardo <makiolo@gmail.com>
2017-08-23 19:42:30 +0200

    Update clean.sh

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -Rf node_modules/depends
-rm -Rf bin
-rm -Rf coverage
-rm -Rf gcc
-rm -Rf clang
+rm -Rf node_modules 2> /dev/null
+rm -Rf bin 2> /dev/null
+rm -Rf coverage 2> /dev/null
+rm -Rf gcc 2> /dev/null
+rm -Rf clang 2> /dev/null
