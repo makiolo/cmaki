4ef9864af1eb37a54f1b00fcd87f5a7ea00aa5f7
Ricardo <makiolo@gmail.com>
2017-08-23 15:58:23 +0200

    Update ci.cmd

--- ci.cmd
+++ ci.cmd
@@ -10,21 +10,30 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 if exist package.json (
   
+  echo [1/3] prepare
+  npm install npm-check-updates
+  call node_modules\.bin\ncu -u
+  
+  echo [2/3] compile
   npm install
   if %errorlevel% neq 0 exit /b %errorlevel%
   
+  echo [3/3] run tests
   npm test
   if %errorlevel% neq 0 exit /b %errorlevel%
 
 ) else (
 
+  echo [1/3] prepare
   if exist node_modules\cmaki (rmdir /s /q node_modules\cmaki)
   powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki/master/bootstrap.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
   if %errorlevel% neq 0 exit /b %errorlevel%
 
+  echo [2/3] compile
   call node_modules\cmaki\install.cmd
   if %errorlevel% neq 0 exit /b %errorlevel%
 
+  echo [3/3] run tests
   call node_modules\cmaki\tests.cmd
   if %errorlevel% neq 0 exit /b %errorlevel%
 
