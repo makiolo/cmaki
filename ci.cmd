df2d9ef64916b111e1610a2b90efb4b8b3645b54
Ricardo <makiolo@gmail.com>
2017-08-23 11:15:01 +0200

    Update ci.cmd

--- ci.cmd
+++ ci.cmd
@@ -6,13 +6,28 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 pip install poster
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-if exist node_modules\cmaki (rmdir /s /q node_modules\cmaki)
-powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki/master/bootstrap.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
-if %errorlevel% neq 0 exit /b %errorlevel%
+if exist package.json (
+  
+  set CMAKI_PWD=%CD%
+  npm install
+  if %errorlevel% neq 0 exit /b %errorlevel%
+  
+  npm test
+  if %errorlevel% neq 0 exit /b %errorlevel%
 
-set CMAKI_PWD=%CD%
-call node_modules\cmaki\install.cmd
-if %errorlevel% neq 0 exit /b %errorlevel%
+) else (
 
-call node_modules\cmaki\tests.cmd
-if %errorlevel% neq 0 exit /b %errorlevel%
+  if exist node_modules\cmaki (rmdir /s /q node_modules\cmaki)
+  powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki/master/bootstrap.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
+  if %errorlevel% neq 0 exit /b %errorlevel%
+
+  set CMAKI_PWD=%CD%
+  call node_modules\cmaki\install.cmd
+  if %errorlevel% neq 0 exit /b %errorlevel%
+
+  call node_modules\cmaki\tests.cmd
+  if %errorlevel% neq 0 exit /b %errorlevel%
+
+)
+
+# TODO: upload artifact (if exist cmaki.yml)
