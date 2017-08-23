42dbed8a6ac0843ad99c9d09d01dd59b4898bce4
Ricardo <makiolo@gmail.com>
2017-08-23 09:59:25 +0200

    Update ci.cmd

--- ci.cmd
+++ ci.cmd
@@ -1,9 +1,19 @@
 @echo off
+
+pip install pyyaml
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+pip install poster
+if %errorlevel% neq 0 exit /b %errorlevel%
+
 if exist node_modules (rmdir /s /q node_modules)
 if %errorlevel% neq 0 exit /b %errorlevel%
+
 powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki/master/bootstrap.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
 if %errorlevel% neq 0 exit /b %errorlevel%
+
 call node_modules\cmaki\install.cmd
 if %errorlevel% neq 0 exit /b %errorlevel%
+
 call node_modules\cmaki\tests.cmd
 if %errorlevel% neq 0 exit /b %errorlevel%
