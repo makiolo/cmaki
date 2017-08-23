dd02127e0c296a4f434afd56e2b13f2976853ce3
Ricardo <makiolo@gmail.com>
2017-08-23 10:24:06 +0200

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -6,6 +6,24 @@ IF "%CMAKI_PWD%" EQU "" (
   echo CMAKI_PWD have value: %CMAKI_PWD%
 )
 
+IF DEFINED CMAKI_PWD (
+  echo CMAKI_PWD is defined
+) ELSE (
+  echo CMAKI_PWD is undefined
+)
+
+IF "%CMAKI_PWD2%" EQU "" (
+  echo CMAKI_PWD2 is empty
+) ELSE (
+  echo CMAKI_PWD2 have value: %CMAKI_PWD2%
+)
+
+IF DEFINED CMAKI_PWD2 (
+  echo CMAKI_PWD2 is defined
+) ELSE (
+  echo CMAKI_PWD2 is undefined
+)
+
 call node_modules\cmaki\clean.cmd
 
 call node_modules\cmaki\setup.cmd
