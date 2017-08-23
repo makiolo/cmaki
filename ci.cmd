db345a919e77b98a76dc2647945c201ce084403b
Ricardo <makiolo@gmail.com>
2017-08-23 10:08:49 +0200

    Update ci.cmd

--- ci.cmd
+++ ci.cmd
@@ -6,12 +6,11 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 pip install poster
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-if exist node_modules (rmdir /s /q node_modules)
-if %errorlevel% neq 0 exit /b %errorlevel%
-
+if exist node_modules\cmaki (rmdir /s /q node_modules\cmaki)
 powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki/master/bootstrap.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
+set CMAKI_PWD=%CD%
 call node_modules\cmaki\install.cmd
 if %errorlevel% neq 0 exit /b %errorlevel%
 
