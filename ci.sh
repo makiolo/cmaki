4947aa9d84c2af6047a54ac873a01ad10368308e
Ricardo <makiolo@gmail.com>
2017-08-23 09:58:25 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
+pip install --user pyyaml
+pip install --user poster
+pip install --user codecov
 bash <(curl -s https://raw.githubusercontent.com/makiolo/cmaki/master/bootstrap.sh)
 CMAKI_PWD=$(pwd) ./node_modules/cmaki/install.sh
 ./node_modules/cmaki/tests.sh
