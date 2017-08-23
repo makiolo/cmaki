740f5276ed12e9c2bb835d2c8c16890d653f3c95
Ricardo <makiolo@gmail.com>
2017-08-23 10:16:07 +0200

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -7,9 +7,9 @@ export CXX="${CXX:-g++}"
 export MODE=${MODE:-Debug}
 
 echo "running in mode $MODE ... ($CC / $CXX)"
-mkdir -p $CC/$MODE
 cd $CC/$MODE
 
 # compile
 cmake --build . --config $MODE --target install -- -j8 -k VERBOSE=1 || cmake --build . --config $MODE --target install -- -j1 VERBOSE=1
-
+code=$?
+exit $code
