50906cb68b92f506390297b7184a523b0fa74c74
Ricardo <makiolo@gmail.com>
2017-08-23 12:13:38 +0200

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -6,18 +6,19 @@ export CMAKI_PWD=$(pwd)
 pip install --user pyyaml
 pip install --user poster
 pip install --user codecov
+
 if [ -f "package.json" ]; then
   npm install -g npm-check-updates
   ncu -u
   npm install
   npm test
 else
-  bash <(curl -s https://raw.githubusercontent.com/makiolo/cmaki/master/bootstrap.sh)
+  curl -s https://raw.githubusercontent.com/makiolo/cmaki/master/bootstrap.sh | bash
   ./node_modules/cmaki/install.sh
   ./node_modules/cmaki/tests.sh
 fi
 
 if [ -f "cmaki.yml" ]; then
-  # interesting autogenerate cmaki.yml from package.json (npm info)
+  # IDEA: interesting autogenerate cmaki.yml from package.json (npm info)
   echo TODO: generate artifact and upload with cmaki_generator (npm run upload)
 fi
