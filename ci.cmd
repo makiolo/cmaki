4fd16b94d47f3f7493fe9af0199f3c056d9a71c0
Ricardo <makiolo@gmail.com>
2017-08-23 11:52:03 +0200

    Update ci.cmd

--- ci.cmd
+++ ci.cmd
@@ -1,5 +1,7 @@
 @echo off
 
+set CMAKI_PWD=%CD%
+
 pip install pyyaml
 if %errorlevel% neq 0 exit /b %errorlevel%
 
@@ -8,7 +10,6 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 
 if exist package.json (
   
-  set CMAKI_PWD=%CD%
   npm install
   if %errorlevel% neq 0 exit /b %errorlevel%
   
@@ -21,7 +22,6 @@ if exist package.json (
   powershell -c "$source = 'https://raw.githubusercontent.com/makiolo/cmaki/master/bootstrap.cmd'; $dest = $env:TEMP + '\bootstrap.cmd'; $WebClient = New-Object System.Net.WebClient; $WebClient.DownloadFile($source,$dest); Invoke-Expression $dest"
   if %errorlevel% neq 0 exit /b %errorlevel%
 
-  set CMAKI_PWD=%CD%
   call node_modules\cmaki\install.cmd
   if %errorlevel% neq 0 exit /b %errorlevel%
 
@@ -30,4 +30,4 @@ if exist package.json (
 
 )
 
-# TODO: upload artifact (if exist cmaki.yml)
+# TODO: upload artifact
