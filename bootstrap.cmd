a3ad5c5a173dfd2383c9773cf14be93072c79393
Ricardo <makiolo@gmail.com>
2017-08-28 22:31:26 +0200

    Update bootstrap.cmd

--- bootstrap.cmd
+++ bootstrap.cmd
@@ -6,3 +6,10 @@ IF EXIST node_modules\cmaki (
   cd node_modules && git clone -q https://github.com/makiolo/cmaki.git && cd ..
   cd node_modules/cmaki && rm -Rf .git && cd ..\..
 )
+IF EXIST node_modules\cmaki_generator (
+  echo .
+) else (
+  md node_modules\cmaki_generator
+  cd node_modules && git clone -q https://github.com/makiolo/cmaki_generator.git && cd ..
+  cd node_modules/cmaki_generator && rm -Rf .git && cd ..\..
+)
