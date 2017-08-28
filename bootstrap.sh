f6695e1b51fe74269a5a05afd6ed5c8459094eba
Ricardo <makiolo@gmail.com>
2017-08-28 22:30:57 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,8 @@ if [[ ! -d node_modules/cmaki ]]; then
 	(cd node_modules && git clone https://github.com/makiolo/cmaki.git)
 	(cd node_modules/cmaki && rm -Rf .git)
 fi
+if [[ ! -d node_modules/cmaki_generator ]]; then
+	mkdir -p node_modules/cmaki_generator
+	(cd node_modules && git clone https://github.com/makiolo/cmaki_generator.git)
+	(cd node_modules/cmaki_generator && rm -Rf .git)
+fi
