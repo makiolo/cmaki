c789d8d76e1baf0df8cfd9f89e33c5dbc8bff3bf
Ricardo <makiolo@gmail.com>
2017-09-04 11:08:40 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,6 +1,3 @@
 #!/bin/bash -e
-
-TRAVIS_REPO_SLUG=makiolo/$(basename $(pwd))
-
-(cd node_modules/cmaki_generator && ./build github://$TRAVIS_REPO_SLUG -o --server=http://artifacts.myftp.biz:8080)
-
+PROJECT_FOLDER=$(pwd)
+(cd node_modules/cmaki_generator && ./build --rootdir=${PROJECT_FOLDER} -o --server=http://artifacts.myftp.biz:8080)
