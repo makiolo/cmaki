9449f43cd9ced3564c9f9ffa1dc4674ce38e61cd
Ricardo <makiolo@gmail.com>
2017-09-04 11:09:12 +0200

    Delete upload_all.sh

--- upload_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -e
-
-TRAVIS_REPO_SLUG=makiolo/$(basename $(pwd))
-
-(cd node_modules/cmaki_generator && CC=gcc CXX=g++ MODE=Debug ./build github://$TRAVIS_REPO_SLUG -o --server=http://artifacts.myftp.biz:8080)
-(cd node_modules/cmaki_generator && CC=gcc CXX=g++ MODE=Release ./build github://$TRAVIS_REPO_SLUG -o --server=http://artifacts.myftp.biz:8080)
-(cd node_modules/cmaki_generator && CC=clang CXX=clang++ MODE=Debug ./build github://$TRAVIS_REPO_SLUG -o --server=http://artifacts.myftp.biz:8080)
-(cd node_modules/cmaki_generator && CC=clang CXX=clang++ MODE=Release ./build github://$TRAVIS_REPO_SLUG -o --server=http://artifacts.myftp.biz:8080)
-
