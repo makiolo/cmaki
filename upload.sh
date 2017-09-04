e764c04bc34079cb4411ea30641a54ec85de1eb5
Ricardo <makiolo@gmail.com>
2017-09-04 11:28:39 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -e
+PACKAGE=$(basename $(pwd))
 YMLFILE=$(pwd)/cmaki.yml
-(cd node_modules/cmaki_generator && ./build --yaml=${YMLFILE} -o --server=http://artifacts.myftp.biz:8080)
+(cd node_modules/cmaki_generator && ./build ${PACKAGE} --yaml=${YMLFILE} -o --server=http://artifacts.myftp.biz:8080)
