0b21e2abce59c646f593add906d5d6872896cc62
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-06 00:31:14 +0200

    minor change

--- install.cmd
+++ install.cmd
@@ -6,6 +6,6 @@ IF DEFINED CMAKI_PWD (
   set CMAKI_PWD=%CD%
 )
 
-call %CMAKI_PWD%\node_modules\cmaki\clean.cmd
 call %CMAKI_PWD%\node_modules\cmaki\setup.cmd || echo error in setup.cmd && exit /b
 call %CMAKI_PWD%\node_modules\cmaki\compile.cmd || echo error in compile.cmd && exit /b
+
--- install.sh
+++ install.sh
@@ -2,13 +2,9 @@
 set -e
 
 export CMAKI_PWD="${CMAKI_PWD:-$PWD}"
-export USE_CMAKI_IDENTIFIER=TRUE
 export MODE="${MODE:-Debug}"
 export CMAKI_IDENTIFIER_FOLDER=$CMAKI_PWD/cmaki_identifier
 export PACKAGE=$(basename $(pwd))
-
-# clean
-$CMAKI_PWD/node_modules/cmaki/clean.sh
 
 # setup
 $CMAKI_PWD/node_modules/cmaki/setup.sh
