4a6bbb4df451256b683f509af9bc29d6a79ee186
Ricardo <makiolo@gmail.com>
2017-09-05 18:18:52 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -2,10 +2,10 @@
 export CMAKI_PWD=$(pwd)
 export PACKAGE=$(basename $CMAKI_PWD)
 export YMLFILE=$CMAKI_PWD/cmaki.yml
+export CMAKI_IDENTIFIER_FOLDER=cmaki_identifier
 
 cd $(pwd)/node_modules/cmaki_generator
 
-CMAKI_IDENTIFIER_FOLDER=cmaki_identifier
 echo ---------- identify platform (in upload) ----------
 if [ ! -d "$CMAKI_IDENTIFIER_FOLDER" ]; then
   git clone https://github.com/makiolo/cmaki_identifier.git $CMAKI_IDENTIFIER_FOLDER
@@ -16,4 +16,4 @@ fi
 $CMAKI_PWD/node_modules/cmaki/ci/$MODE/predef
 echo ---------------------------------------
 
-./build ${PACKAGE} --yaml=${YMLFILE} -o --server=http://artifacts.myftp.biz:8080
+./build ${PACKAGE} --yaml=${YMLFILE} --server=http://artifacts.myftp.biz:8080
