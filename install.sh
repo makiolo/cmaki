c38bc6bf94b414f02420db89c387edfdd7da7670
Ricardo <makiolo@gmail.com>
2017-09-05 11:16:48 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,20 +5,23 @@ export CMAKI_PWD="${CMAKI_PWD:-$PWD}"
 export USE_CMAKI_IDENTIFIER=TRUE
 export MODE="${MODE:-Debug}"
 export CMAKI_IDENTIFIER_FOLDER=$CMAKI_PWD/cmaki_identifier
+export PACKAGE=$(basename $(pwd))
 
 # clean
 $CMAKI_PWD/node_modules/cmaki/clean.sh
 
 # identify
-echo ---------- identify platform ----------
-if [ ! -d "$CMAKI_IDENTIFIER_FOLDER" ]; then
-  git clone https://github.com/makiolo/cmaki_identifier.git $CMAKI_IDENTIFIER_FOLDER
-  cd $CMAKI_IDENTIFIER_FOLDER
-  CMAKI_INSTALL=$CMAKI_PWD/node_modules/cmaki/ci npm install
-  cd -
+if [ "$PACKAGE" != "cmaki_identifier" ]; then
+  echo ---------- identify platform ----------
+  if [ ! -d "$CMAKI_IDENTIFIER_FOLDER" ]; then
+    git clone https://github.com/makiolo/cmaki_identifier.git $CMAKI_IDENTIFIER_FOLDER
+    cd $CMAKI_IDENTIFIER_FOLDER
+    CMAKI_INSTALL=$CMAKI_PWD/node_modules/cmaki/ci npm install
+    cd -
+  fi
+  $CMAKI_PWD/node_modules/cmaki/ci/$MODE/predef
+  echo ---------------------------------------
 fi
-$CMAKI_PWD/node_modules/cmaki/ci/$MODE/predef
-echo ---------------------------------------
 
 # setup
 $CMAKI_PWD/node_modules/cmaki/setup.sh
