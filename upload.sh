dec516c1a82c3afa7db8b2be7c2e90a083b8bc7c
Ricardo <makiolo@gmail.com>
2017-09-05 18:23:19 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -4,16 +4,18 @@ export PACKAGE=$(basename $CMAKI_PWD)
 export YMLFILE=$CMAKI_PWD/cmaki.yml
 export CMAKI_IDENTIFIER_FOLDER=cmaki_identifier
 
+mkdir -p node_modules/cmaki
+
 cd $(pwd)/node_modules/cmaki_generator
 
 echo ---------- identify platform (in upload) ----------
 if [ ! -d "$CMAKI_IDENTIFIER_FOLDER" ]; then
   git clone https://github.com/makiolo/cmaki_identifier.git $CMAKI_IDENTIFIER_FOLDER
-  cd $CMAKI_IDENTIFIER_FOLDER
-  CMAKI_PWD=$(pwd) CMAKI_INSTALL=$CMAKI_IDENTIFIER_FOLDER/../node_modules/cmaki/ci npm install
+  cd $CMAKI_IDENTIFIER_FOLDER # enter $(pwd)/node_modules/cmaki_generator/cmaki_identifier
+  CMAKI_PWD=$(pwd) CMAKI_INSTALL=$CMAKI_IDENTIFIER_FOLDER/../../cmaki/ci npm install
   cd -
 fi
 $CMAKI_PWD/node_modules/cmaki/ci/$MODE/predef
-echo ---------------------------------------
+echo ----------------------------------------------------
 
 ./build ${PACKAGE} --yaml=${YMLFILE} --server=http://artifacts.myftp.biz:8080
