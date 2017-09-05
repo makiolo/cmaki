0b083b54cb2ccc8ed91c74d4480e83f6d274eb6e
Ricardo <makiolo@gmail.com>
2017-09-05 17:40:59 +0200

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -6,7 +6,7 @@ cd $(pwd)/node_modules/cmaki_generator
 
 git clone https://github.com/makiolo/cmaki_identifier.git
 cd cmaki_identifier
-CMAKI_PWD=$(pwd) CMAKI_INSTALL=$(pwd)/../node_modules/cmaki/ci npm install
+CMAKI_PWD=$(pwd) CMAKI_INSTALL=$(pwd)/node_modules/cmaki/ci npm install
 cd -
 
 ./build ${PACKAGE} --yaml=${YMLFILE} -o --server=http://artifacts.myftp.biz:8080
