dead72dbfda7a4a741bdac9ab767edd3e524ed07
Ricardo <makiolo@gmail.com>
2017-09-13 19:33:39 +0200

    Update detect_operative_system.sh

--- ci/detect_operative_system.sh
+++ ci/detect_operative_system.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 export CMAKI_INSTALL="${CMAKI_INSTALL:-$CMAKI_PWD/bin}"
 export PATH=$CMAKI_INSTALL:$PATH
-cmaki_identifier
+export COMPILER_BASENAME=$(basename ${CC})
 
+cmaki_identifier > /dev/null 2>&1
+if [ $? != 0 ]; then
+  echo $COMPILER_BASENAME
+else
+  cmaki_identifier
+fi
