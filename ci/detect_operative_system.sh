ce5f8d610bd83e0e264d9ef69897f619bcbf417b
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-14 21:49:09 +0200

    .

--- ci/detect_operative_system.sh
+++ ci/detect_operative_system.sh
@@ -5,9 +5,6 @@ export CXX="${CXX:-g++}"
 export MODE="${MODE:-Debug}"
 export CMAKI_INSTALL="${CMAKI_INSTALL:-$CMAKI_PWD/bin}"
 export CMAKI_EMULATOR="${CMAKI_EMULATOR:-}"
-# export PATH=$CMAKI_INSTALL:$PATH
 
-ls $CMAKI_INSTALL
-echo $CMAKI_EMULATOR $CMAKI_INSTALL/cmaki_identifier
-echo $CMAKI_EMULATOR $CMAKI_INSTALL/cmaki_identifier
+wine $CMAKI_INSTALL/cmaki_identifier
 
