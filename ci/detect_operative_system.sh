b946d1e30694a105e210142266fe0e0086412497
Ricardo Marmolejo García <makiolo@gmail.com>
2017-09-14 22:40:12 +0200

    .

--- ci/detect_operative_system.sh
+++ ci/detect_operative_system.sh
@@ -6,7 +6,7 @@ export MODE="${MODE:-Debug}"
 export CMAKI_INSTALL="${CMAKI_INSTALL:-$CMAKI_PWD/bin}"
 export CMAKI_EMULATOR="${CMAKI_EMULATOR:-}"
 
-if [[ "$WINEARCH" == "win32" ]]; then
+if [[ "$WINEARCH" = "win32" ]]; then
 	wine $CMAKI_INSTALL/cmaki_identifier.exe
 else
 	$CMAKI_EMULATOR $CMAKI_INSTALL/cmaki_identifier
