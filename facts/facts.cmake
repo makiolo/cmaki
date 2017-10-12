cc1237b88812b62997d5c87dc76cf66bfa2b55db
Ricardo <makiolo@gmail.com>
2017-10-12 21:36:00 +0200

    Update facts.cmake

--- facts/facts.cmake
+++ facts/facts.cmake
@@ -560,7 +560,7 @@ function(cmaki_test)
 			add_test(
 				NAME ${_TEST_NAME}${_TEST_SUFFIX}
 				COMMAND $<TARGET_FILE:${_TEST_NAME}${_TEST_SUFFIX}> --gmock_verbose=error
-				WORKING_DIRECTORY $ENV{CMAKI_INSTALL}
+				WORKING_DIRECTORY $ENV{CMAKI_INSTALL}/${BUILD_TYPE}
 				CONFIGURATIONS ${BUILD_TYPE})
 		else()
 			add_test(
