97da5f89bc7a6cc9e87c878b3ed1336b2720c097
Ricardo Marmolejo García <makiolo@gmail.com>
2018-02-21 00:05:28 +0100

    .

--- facts/facts.cmake
+++ facts/facts.cmake
@@ -159,7 +159,7 @@ function(cmaki_find_package)
 		# pero queremos que evite compilar cosas que estan en cache remota
 		#
 		execute_process(
-			COMMAND python ${ARTIFACTS_PATH}/build.py ${PACKAGE} --depends=${DEPENDS_PATHFILE} --cmakefiles=${CMAKI_PATH} --prefix=${DEPENDS_PATH} --third-party-dir=${CMAKE_PREFIX_PATH} --server=${CMAKI_REPOSITORY} -o
+			COMMAND python ${ARTIFACTS_PATH}/build.py ${PACKAGE} --depends=${DEPENDS_PATHFILE} --cmakefiles=${CMAKI_PATH} --prefix=${DEPENDS_PATH} --third-party-dir=${CMAKE_PREFIX_PATH} --server=${CMAKI_REPOSITORY} --no-purge -o
 			WORKING_DIRECTORY "${ARTIFACTS_PATH}"
 			RESULT_VARIABLE artifacts_result
 			)
